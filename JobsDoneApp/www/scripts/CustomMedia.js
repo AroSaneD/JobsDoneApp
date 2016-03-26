326ef5b5f411bd805a9149dccf56418c6304534b
Arūnas Seniucas <Arnas Seniucas>
2016-03-26 10:53:17 +0200

    Fixed audio playback and removed debug alerts

--- JobsDoneApp/www/scripts/CustomMedia.js
+++ JobsDoneApp/www/scripts/CustomMedia.js
@@ -2,30 +2,30 @@
 
 var loadMedia = function () {
 	var src = "/media/JobsDone.mp3";
-	if (device.platform == device.android) {
+
+	if (device.platform == "Android") { //Documentation lied. device.android returns undefined.
 		src = "/android_asset/www" + src;
 	}
 
-	myMedia = new Media(src, loadSucceeded, loadFailed, onStatus);
+	myMedia = new Media(src, loadSucceeded, loadFailed, onStatus); //Bug or undocumented behavior - the onload methods (both successfull and not) kept getting called everytime the audio was played.
 }
 
 var playMedia = function () {
-	if (my_media != null) {
-		my_media.play();
+	if (myMedia != null) {
+		myMedia.play();
 	}
 }
 
 var loadSucceeded = function () {
-	alert("Succeeded to load media");
+
 }
 
 var loadFailed = function () {
-	alert("Failed to load media");
+
 }
 
 var onStatus = function (status) {
 	if (status == Media.MEDIA_STOPPED) {
-		my_media.stop();
-		alert("Media stopped");
+		myMedia.stop(); //Bug: if not told to stop, the audio kept repeating over and over (only seen on android devices)
 	}
 }
