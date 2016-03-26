6f7b5edc7aaefa4b8fb94ae0c5b4ebc1cf8fccf2
Arūnas Seniucas <Arnas Seniucas>
2016-03-26 09:50:26 +0200

    First commit

---
+++ JobsDoneApp/www/scripts/CustomMedia.js
@@ -0,0 +1,31 @@
+﻿var myMedia = null;
+
+var loadMedia = function () {
+	var src = "/media/JobsDone.mp3";
+	if (device.platform == device.android) {
+		src = "/android_asset/www" + src;
+	}
+
+	myMedia = new Media(src, loadSucceeded, loadFailed, onStatus);
+}
+
+var playMedia = function () {
+	if (my_media != null) {
+		my_media.play();
+	}
+}
+
+var loadSucceeded = function () {
+	alert("Succeeded to load media");
+}
+
+var loadFailed = function () {
+	alert("Failed to load media");
+}
+
+var onStatus = function (status) {
+	if (status == Media.MEDIA_STOPPED) {
+		my_media.stop();
+		alert("Media stopped");
+	}
+}
